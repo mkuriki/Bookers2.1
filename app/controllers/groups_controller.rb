e33800cafde305ab14c8e91f2b048c93113d64c3
mkuriki <101915061+mkuriki@users.noreply.github.com>
2022-05-16 09:50:00 +0000

    [Fix]グループ作成機能-エラー修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,52 +2,53 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
 
+  def new
+    @group =Group.new
+  end
+
   def index
-      @books = Book.all
-      @groups = Group.all
-      @group = Group.new
+    @groups = Group.all
+    @book = Book.new
   end
 
   def show
-      @group = Group.find(params[:id])
+    @group = Group.find(params[:id])
   end
 
   def create
-      @group = Group.new(group_params)
-      
-      if @group.save
-        redirect_to groups_path
-        flash[:notice] = "You have created book successfully."
-      else
-        @books = Book.all
-        @groups = Group.all
-        render :index
-      end
-      
+    @group = Group.new(group_params)
+    @group.owner_id = current_user
+    if @group.save
+      redirect_to groups_path
+      flash[:notice] = "You have created group successfully."
+    else
+      render 'new'
+    end
+    
   end
 
   def destroy
-      @group = Group.find(params[:id])
-      @group.destroy
-      redirect_to '/groups'
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to '/groups'
   end
 
   def update
-      @group = Group.find(params[:id])
-      @group.update
-      redirect_to '/groups'
+    @group = Group.find(params[:id])
+    @group.update
+    redirect_to '/groups'
   end
 
   private
 
   def group_params
-      params.require(:group).permit(:name, :introduction) 
+    params.require(:group).permit(:name, :introduction) 
   end
 
   def ensure_correct_user
-      @group = Group.find(params[:id])
-      unless @group.owner_id == current_user.id
-        redirect_to groups_path
-      end
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
   end
 end
